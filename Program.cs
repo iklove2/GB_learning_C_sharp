b41058a791abc12a5fec07fee663aac198f3a5a3
iklove2 <iklove@yandex.ru>
2022-08-09 20:41:51 +0300

    Сделана генерация массива

---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿/*
+Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+длина которых меньше либо равна 3 символа. 
+Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+Примеры:
+
+["hello", "2", "world", ":-)"] -> ["2", ":-)"]
+["1234", "1567", "-2", "computer science"] -> ["-2"]
+["Russia", "Denmark", "Kazan"] -> []
+*/
+string GenerateWord(int lengthArray)//Генерируем слово произвольной длины
+{
+    Random rnd = new Random();
+    char[] wordOfArray = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 
+                           'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 
+                           's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1',
+                           '2', '3', '4', '5', '6', '7', '8', '9', '0',
+                           '[', ']', '{', '}', ';', ':', '\'', '\"', '<',
+                           '>', ',', '.', '/', '\\', '!', '@', '#', '$',
+                           '%', '^', '&', '*', '(', ')', '-', '+', '=',
+                           '?', '~', '`'};
+    string result = string.Empty;
+    int element = 0;
+    int lengthOfWord = wordOfArray.Length;
+    for (var i = 0; i < lengthArray; i++)
+    {
+        element = rnd.Next(1, lengthOfWord-1);
+        result = result + wordOfArray[element];
+        Console.Write(wordOfArray[element]);
+    }
+    return result;
+}
+
+string[] GenerateArray(int length, int maxWordLength = 10, int minWordLength = 1)//Генерируем массив произвольной длины из произвольных слов
+{
+    string[] result = new string[length];
+    int wordLength = 0;
+    for (int i = 0; i < length; i++)
+    {
+        wordLength = new Random().Next(minWordLength, maxWordLength);
+        result[i] = GenerateWord(wordLength);
+    }
+    return result;
+}
+
+void printColor(string message, ConsoleColor color = ConsoleColor.Green)
+{
+    Console.ForegroundColor = color;
+    Console.Write($"{message}");
+    Console.ResetColor();
+}
+
+void printArray(string[] arrayToPrint)
+{
+    int length = arrayToPrint.Length;
+    Console.WriteLine();
+    printColor("[", ConsoleColor.DarkYellow);
+    for (int i = 0; i < length - 1; i++)
+    {
+        printColor("\"", ConsoleColor.DarkBlue);
+        printColor($"{arrayToPrint[i]}", ConsoleColor.Green);
+        printColor("\"", ConsoleColor.DarkBlue);
+        printColor(", ", ConsoleColor.DarkYellow);
+        //Console.WriteLine();
+    }
+    printColor($"\"{arrayToPrint[length - 1]}\"", ConsoleColor.DarkCyan);
+    printColor("]", ConsoleColor.DarkYellow);
+}
+
+string[] ResultArray(string[] arrayOfWorld, int minChar = 3)
+{
+    string[] result = new string[1];
+    
+
+}
+
+void main()
+{
+    Console.Write("Введите максимальный размер слова: ");
+    int maxWordLength = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите количество элементов массива: ");
+    int lengthArray = Convert.ToInt32(Console.ReadLine());
+    string[] arrayOfWorld = GenerateArray(lengthArray, maxWordLength, 1);
+
+    printArray(arrayOfWorld);
+}
+
+main();
+
+
+
+
+
+
+
+
+
